5b3545bf7b592bcb67d82236a20a39268fe67675
yeondol <97437403+Min9807@users.noreply.github.com>
2023-10-13 12:57:48 +0900

    [CHORE]정리

--- src/test/java/ot/batch/BatchApplicationTests.java
+++ src/test/java/ot/batch/BatchApplicationTests.java
@@ -1,20 +1,8 @@
 package ot.batch;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ot.batch.api.open.service.OpenApiService;
 
 @SpringBootTest
 class BatchApplicationTests {
-
-	@Autowired
-	private OpenApiService openApiService;
-
-	@Test
-	void contextLoads() {
-		System.out.println(openApiService.requestAreaBased(12, 3, 1));
-
-	}
 
 }
